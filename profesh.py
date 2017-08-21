ffbf7f0b684ffedaec2aef143d71172886f9e449
Margaret Gorguissian <Margaret.Gorguissian@tufts.edu>
2017-08-21 11:50:35 -0400

    retweeting links only

--- profesh.py
+++ profesh.py
@@ -9,11 +9,31 @@ from datetime import datetime
 
 from api import get_API
 
-TWITTER_NAME = "" #place the twitter handle you would like to take tweets from here
+TWITTER_NAME = "arshile_gorky" #place the twitter handle you would like to take tweets from here
 TWEET_WINDOW = True
 FOLLOWERS = []
+SWEAR_WORDS = ["fuck", "bitch", "shit", "cunt", "ass"]
+API = get_API()
 
 def processTweets():
-    recent_tweets = API.user_timeline([TWITTER_NAME], [since_id], [max_id], [count], [page])
+    recent_tweets = API.user_timeline(TWITTER_NAME, count = 16, include_rts = True)
+    links = []
     for tweet in recent_tweets:
-        if not tweet.retweeted and not 
+        if hasattr(tweet, 'entities'):
+            if tweet.entities['urls']:
+                if hasattr(tweet, 'retweeted_status'):
+                    API.retweet(tweet.id)
+                else: 
+                    for url in tweet.entities['urls']:
+                        links.append(url['expanded_url'])    
+
+def professional_tweet(tweet):
+    #if tweet contains swear words
+         #return false
+    #if tweet contains bad phrases
+         #return false
+    #if tweet contains sensitive content
+         #return false
+    #if tweet contains 
+    #return true
+processTweets() 
