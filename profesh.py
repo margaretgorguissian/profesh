08d06cb6de2bbfee0618e7d5385a278ed36ce2b9
Margaret Gorguissian <Margaret.Gorguissian@tufts.edu>
2017-08-21 16:43:17 -0400

    added scheduler

--- profesh.py
+++ profesh.py
@@ -12,15 +12,14 @@ from api import get_API
 
 TWITTER_NAME = "arshile_gorky" #place the twitter handle you would like to take tweets from here
 TWEET_WINDOW = True
-FOLLOWERS = []
+LINKS = []
 SWEAR_WORDS = re.compile('fuck|bitch|shit|cunt|dick|pussy')
 # Here are just some "unprofessional" twitters I frequequent retweet from. Gonna come up with a cleaner way to do this later.
 FORBIDDEN_USERS = ["ClickHole", "Reductress", "iamcardib", "GlowNetflix", "Raquel_Savage", "claudiachoxo", "HAUTEVIOLENCE"]
 API = get_API()
 
-def processTweets():
+def process_tweets():
     recent_tweets = API.user_timeline(TWITTER_NAME, count = 8, include_rts = True)
-    links = []
     for tweet in recent_tweets:
         post = professional_tweet(tweet)
         try:
@@ -30,7 +29,7 @@ def processTweets():
                       print("Tweet retweeted")
                  else:
                       for url in post.entities['urls']:
-                          links.append(url['expand_url'])
+                          LINKS.append(url['expand_url'])
         except AttributeError:
              print("Tweet does not contain link.")
 
@@ -47,4 +46,18 @@ def professional_tweet(tweet):
     except AttributeError:
        return tweet
 
-processTweets() 
+def post_list:
+    print LINKS
+    LINKS.clear()
+
+def retweet(tweet_id):
+    API.retweet(tweet_id)
+
+def post_tweet(text):
+    API.update_status(text)
+
+tweet_scheduler = BackgroundScheduler()
+# I tweet a lot. Every 20 minutes, go through my tweets and check if there's anything new to retweet
+tweet_scheduler.add_job(process_tweets, 'interval', minutes=20, id='process_tweets')
+# At midnight, post a list of all links I posted myself, so I can decide what I want to post on my other account.
+tweet_scheduler.add_job(post_list, 'cron', hour=00, id='post_list')
